5b9b218b6b23f3d1b555d548de76285dec1c914e
Nishant Amoli <nishantamoli@gmail.com>
2018-01-14 23:09:04 +0530

    Add files via upload

---
+++ BasicExceptions.java
@@ -0,0 +1,19 @@
+package testExceptions;
+import java.io.*;
+public class BasicExceptions {
+	public static void main(String args[])throws IOException
+	{
+		int no=0;
+		System.out.print("Demonstration of Exception Handling in JAVA.\n@Nishant Amoli,B.Tech IT");
+		System.out.println("\nEnter a number");
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		no=Integer.parseInt(br.readLine());
+		try
+		{
+			if(no>0)System.out.print("\nNumber accepted.");
+			else
+				throw new Exception("Number should not be negative");			
+		}catch(Exception e){System.out.print(e);}
+		System.out.print("\n"+no);
+	}
+}
